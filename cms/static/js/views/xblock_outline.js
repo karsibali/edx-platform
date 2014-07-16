ad41b82b3dd4e9ac4f1d16e42cf444203a52e940
Alexander Kryklia <kryklia@gmail.com>
2014-07-16 10:25:11 +0300

    Move new method to course_outline view.

--- cms/static/js/views/xblock_outline.js
+++ cms/static/js/views/xblock_outline.js
@@ -15,9 +15,8 @@
  *  - edit_display_name - true if the shown xblock's display name should be in inline edit mode
  */
 define(["jquery", "underscore", "gettext", "js/views/baseview", "js/views/utils/view_utils",
-        "js/views/utils/xblock_utils", "js/views/xblock_string_field_editor",
-         "js/views/modals/edit_section_in_outline"],
-    function($, _, gettext, BaseView, ViewUtils, XBlockViewUtils, XBlockStringFieldEditor, EditSectionXBlockModal) {
+        "js/views/utils/xblock_utils", "js/views/xblock_string_field_editor"],
+    function($, _, gettext, BaseView, ViewUtils, XBlockViewUtils, XBlockStringFieldEditor) {
 
         var XBlockOutlineView = BaseView.extend({
             // takes XBlockInfo as a model
@@ -142,10 +141,6 @@ define(["jquery", "underscore", "gettext", "js/views/baseview", "js/views/utils/
                     self.deleteXBlock($(event.target));
                 });
                 element.find('.add-button').click(_.bind(this.handleAddEvent, this));
-                element.find('.configure-button').click(function(event) {
-                    event.preventDefault();
-                    self.editXBlock($(event.target));
-                });
             },
 
             shouldRenderChildren: function() {
@@ -235,15 +230,7 @@ define(["jquery", "underscore", "gettext", "js/views/baseview", "js/views/utils/
                 });
             },
 
-            isEditable: function() {
-                return _.contains(['sequential', 'chapter'], this.model.get('category'));
-            },
 
-            editXBlock: function() {
-                var modal;
-                modal = new EditSectionXBlockModal(this.model);
-                modal.show();
-            },
 
             /**
              * Refresh the view's model from the server, which will cause the view to refresh.
